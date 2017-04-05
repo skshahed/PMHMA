683cf7c116bc87248700e1cef299b69f73eaaa54
Shahed <sha_hed007@yahoo.com>
2017-04-05 17:30:41 +0600

    Add ,login, show doctor

--- app/src/main/java/com/example/forever/pmhma/MainActivity.java
+++ app/src/main/java/com/example/forever/pmhma/MainActivity.java
@@ -4,21 +4,68 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText nameET;
+    private EditText detailsET;
+    private EditText appoinmentET;
+    private EditText phoneET;
+    private EditText emailET;
 
-    private Button goLoginBtn;
+    private Doctor doctor;
+    private DoctorDatabaseSource doctorDatabaseSource;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        goLoginBtn = (Button) findViewById(R.id.goLogin);
+        nameET        = (EditText) findViewById(R.id.doctorName);
+        detailsET     = (EditText) findViewById(R.id.doctorDetails);
+        appoinmentET  = (EditText) findViewById(R.id.doctorAppoinment);
+        phoneET       = (EditText) findViewById(R.id.doctorPhone);
+        emailET       = (EditText) findViewById(R.id.doctorEmail);
+        doctorDatabaseSource = new DoctorDatabaseSource(this);
     }
 
-    public void goLogin(View view) {
+    public void addDoctor(View view) {
+        String name         =   nameET.getText().toString();
+        String details      =   detailsET.getText().toString();
+        String appointment  =   appoinmentET.getText().toString();
+        String phone        =   phoneET.getText().toString();
+        String email        =   emailET.getText().toString();
+        if(name.isEmpty()){
+            nameET.setError("This field must not be required !");
+        }
+        if(details.isEmpty()){
+            detailsET.setError("This field must not be required !");
+        }
+
+        if(appointment.isEmpty()){
+            appoinmentET.setError("This field must not be required !");
+        }
+
+        if(phone.isEmpty()){
+            phoneET.setError("This field must not be required !");
+        }
+
+        if(email.isEmpty()){
+            emailET.setError("This field must not be required !");
+        }else{
+            doctor =   new Doctor(name,details,appointment,phone,email);
+            boolean status  =   doctorDatabaseSource.addDoctorInfo(doctor);
+            if(status){
+                Toast.makeText(this, "Successfull", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this,DoctorListActivity.class));
+            }else{
+                Toast.makeText(this, "Could not save", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void addRegistration(View view) {
         Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
-
     }
 }
